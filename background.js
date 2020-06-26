90a6e7d7b0d05eaf89401aa157f77c0c378ae472
blackcolt <idan.magled@gmail.com>
2020-06-26 14:37:59 +0300

    - Added icon and badge logic.

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
+chrome.storage.sync.get("isExtensionActive", function (obj) {
+    chrome.browserAction.setBadgeText({text: obj.isExtensionActive.toString()});
+});
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.storage.sync.get('isExtensionActive', storage => {
         chrome.storage.sync.set({
             isExtensionActive: !storage.isExtensionActive,
         });
+        chrome.browserAction.setBadgeText({text: !storage.isExtensionActive.toString()});
     });
 });
